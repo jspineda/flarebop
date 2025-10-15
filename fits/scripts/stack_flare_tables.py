21b003f3da17baeb101fe2725890b1ea99b98b2d
parke <astroparke@gmail.com>
2025-10-15 11:31:15 -0700

    fixed flare contrast definition in tables and fits

--- fits/scripts/stack_flare_tables.py
+++ fits/scripts/stack_flare_tables.py
@@ -15,7 +15,7 @@ Fcat['sample'] = 'active'
 Hcat['sample'] = '40Myr'
 cat = table.vstack((Mcat, Fcat, Hcat))
 
-# standardize column names so there aren't special cases to handle later
+#%% standardize column names so there aren't special cases to handle later
 replacement_map = {
     'lya_broad' : 'lya wings',
     'PEW' : 'equiv dur',
@@ -31,6 +31,30 @@ for colname in cat.colnames:
     cat.rename_column(colname, newname)
 
 
+#%% correct contrast values
+# original constrast values were Fpk/Fq + 1. The +1 was a mistake. This work will define contrast as
+# Fpk/Fq - 1, so that the minimum flare contrast is 0.
+#
+# this also messed up the uncertainties. Some math to correct those too:
+# r = p/q + 1 %original contrast definition, r is contrast, p peak flux, q quiescent flux
+#
+# s_r = r * √((s_p/p)2 + (s_q/q)2)  %what Loyd 2018a,b used to compute uncty, wrong in several ways (sigh)
+#
+# r' = p/q - 1 % how we're defining contrast here
+# s_r' = √((s_p/q)2 + (s_q*p/q2)2) = p/q * √((s_p/p)2 + (s_q/q)2) = (r-1) * s_r/r = (r-1)/r * s_r
+
+contrast_names = [name for name in cat.colnames if 'contrast' in name and 'err' not in name]
+for name in contrast_names:
+    r = cat[name]
+    ename = name.replace('contrast', 'contrast_err')
+    e = cat[ename]
+    rr = r - 2
+    ee = (r - 1) / r * e
+    cat[name] = rr
+    cat[ename] = ee
+
+
+#%% sort and save
 cat.sort('equiv dur-fuv130') # equivalent duration (photometric equivalent width)
 
 cat.write('fits/data/all_flares_compiled.ecsv', overwrite=True)
